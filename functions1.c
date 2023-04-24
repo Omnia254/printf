bb1c54fbb5900a021c248779196e04a063e845bb
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2023-04-24 22:57:20 +0200

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -15,19 +15,19 @@ int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int n = va_arg(types, unsigned long int);
 
-	num = convert_size_unsgnd(num, size);
+	n = convert_size_unsgnd(n, size);
 
-	if (num == 0)
+	if (n == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (n > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
+		buffer[i--] = (n % 10) + '0';
+		n /= 10;
 	}
 
 	i++;
@@ -51,22 +51,22 @@ int print_octal(va_list types, char buffer[],
 {
 
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
+	unsigned long int n = va_arg(types, unsigned long int);
+	unsigned long int init_num = n;
 
 	UNUSED(width);
 
-	num = convert_size_unsgnd(num, size);
+	n = convert_size_unsgnd(num, size);
 
-	if (num == 0)
+	if (n == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (n > 0)
 	{
-		buffer[i--] = (num % 8) + '0';
-		num /= 8;
+		buffer[i--] = (n % 8) + '0';
+		n /= 8;
 	}
 
 	if (flags & F_HASH && init_num != 0)
@@ -123,7 +123,6 @@ int print_hexa_upper(va_list types, char buffer[],
  * @flag_ch: Calculates active flags
  * @width: get width
  * @precision: Precision specification
- * @size: Size specifier
  * @size: Size specification
  * Return: Number of chars printed
  */
@@ -131,22 +130,22 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
-	unsigned long int init_num = num;
+	unsigned long int n = va_arg(types, unsigned long int);
+	unsigned long int init_num = n;
 
 	UNUSED(width);
 
-	num = convert_size_unsgnd(num, size);
+	n = convert_size_unsgnd(n, size);
 
-	if (num == 0)
+	if (n == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (n > 0)
 	{
-		buffer[i--] = map_to[num % 16];
-		num /= 16;
+		buffer[i--] = map_to[n % 16];
+		n /= 16;
 	}
 
 	if (flags & F_HASH && init_num != 0)
