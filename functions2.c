3a39f382b8d16315916acdb12605f2e6764eae76
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-24 23:07:09 +0200

    edits for functions 2

--- functions2.c
+++ functions2.c
@@ -14,42 +14,42 @@
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	char extra_c = 0, padd = ' ';
-	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
-	unsigned long num_addrs;
-	char map_to[] = "0123456789abcdef";
-	void *addrs = va_arg(types, void *);
+	char extra_char = 0, padd = ' ';
+	int indent = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long n_address;
+	char map_chars[] = "0123456789abcdef";
+	void *address = va_arg(types, void *);
 
 	UNUSED(width);
 	UNUSED(size);
 
-	if (addrs == NULL)
+	if (address == NULL)
 		return (write(1, "(nil)", 5));
 
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
 
-	num_addrs = (unsigned long)addrs;
+	n_address = (unsigned long)address;
 
-	while (num_addrs > 0)
+	while (n_address > 0)
 	{
-		buffer[ind--] = map_to[num_addrs % 16];
-		num_addrs /= 16;
+		buffer[indent--] = map_chars[n_address % 16];
+		n_address /= 16;
 		length++;
 	}
 
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
-		extra_c = '+', length++;
+		extra_char = '+', length++;
 	else if (flags & F_SPACE)
-		extra_c = ' ', length++;
+		extra_char = ' ', length++;
 
-	ind++;
+	indent++;
 
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
-	return (write_pointer(buffer, ind, length,
-		width, flags, padd, extra_c, padd_start));
+	return (write_pointer(buffer, indent, length,
+		width, flags, padd, extra_char, padd_start));
 }
 
 /************************* PRINT NON PRINTABLE *************************/
