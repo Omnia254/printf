afcebec9a236a64c580f05c4453e06f08e87189c
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2023-04-24 22:59:59 +0200

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -15,19 +15,19 @@ int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
-	unsigned long int num = va_arg(types, unsigned long int);
+	unsigned long int n = va_arg(types, unsigned long int);
 
-	num = convert_size_unsgnd(num, size);
+	n = convert_size_unsgnd(n, size);
 
-	if (num == 0)
+	if (n == 0)
 		buffer[i--] = '0';
 
 	buffer[BUFF_SIZE - 1] = '\0';
 
-	while (num > 0)
+	while (n > 0)
 	{
-		buffer[i--] = (num % 10) + '0';
-		num /= 10;
+		buffer[i--] = (n % 10) + '0';
+		n /= 10;
 	}
 
 	i++;
