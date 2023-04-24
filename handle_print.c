5434f17cef1ac7bcb4e2705d90e62fbbe41dffc2
Omnia254 <76522001+Omnia254@users.noreply.github.com>
2023-04-24 22:13:32 +0200

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -6,13 +6,13 @@
  * @ind: ind.
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
- * @gwidth: get width.
- * @gprecision: Precision specification
- * @gsize: Size specifier
+ * @width: get width.
+ * @precision: Precision specification
+ * @size: Size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int gwidth, int gprecision, int gsize)
+	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
@@ -24,7 +24,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, gwidth, gprecision, gsize));
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
@@ -33,7 +33,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
 			unknow_len += write(1, " ", 1);
-		else if (gwidth)
+		else if (width)
 		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
