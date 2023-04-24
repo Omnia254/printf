878c747600933f30f103894683536d6d022f4f54
Hoda <hs@Hodas-MacBook-Air.local>
2023-04-24 23:37:53 +0200

    edits for functions 2

--- functions2.c
+++ functions2.c
@@ -14,50 +14,50 @@
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	char extra_char = 0, padd = ' ';
-	int indent = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
-	unsigned long n_address;
-	char map_chars[] = "0123456789abcdef";
-	void *address = va_arg(types, void *);
+	char extra_c = 0, padd = ' ';
+	int ind = BUFF_SIZE - 2, length = 2, padd_start = 1; /* length=2, for '0x' */
+	unsigned long num_addrs;
+	char map_to[] = "0123456789abcdef";
+	void *addrs = va_arg(types, void *);
 
 	UNUSED(width);
 	UNUSED(size);
 
-	if (address == NULL)
+	if (addrs == NULL)
 		return (write(1, "(nil)", 5));
 
 	buffer[BUFF_SIZE - 1] = '\0';
 	UNUSED(precision);
 
-	n_address = (unsigned long)address;
+	num_addrs = (unsigned long)addrs;
 
-	while (n_address > 0)
+	while (num_addrs > 0)
 	{
-		buffer[indent--] = map_chars[n_address % 16];
-		n_address /= 16;
+		buffer[ind--] = map_to[num_addrs % 16];
+		num_addrs /= 16;
 		length++;
 	}
 
 	if ((flags & F_ZERO) && !(flags & F_MINUS))
 		padd = '0';
 	if (flags & F_PLUS)
-		extra_char = '+', length++;
+		extra_c = '+', length++;
 	else if (flags & F_SPACE)
-		extra_char = ' ', length++;
+		extra_c = ' ', length++;
 
-	indent++;
+	ind++;
 
 	/*return (write(1, &buffer[i], BUFF_SIZE - i - 1));*/
-	return (write_pointer(buffer, indent, length,
-		width, flags, padd, extra_char, padd_start));
+	return (write_pointer(buffer, ind, length,
+		width, flags, padd, extra_c, padd_start));
 }
 
 /************************* PRINT NON PRINTABLE *************************/
 /**
  * print_non_printable - Prints ascii codes in hexa of non printable chars
- * @types: List of arguments
+ * @types: Lista of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculate active flags
+ * @flags:  Calculates active flags
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
